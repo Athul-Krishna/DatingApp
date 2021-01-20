172a474cd09cc3455b7daaa69cc13a33cb0c6e56
Athul <athulkrishnaks1998@gmail.com>
2021-01-20 10:52:55 +0530

    Building a great user interface

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { appRoutes } from './routes';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
- 
+import { JwtModule } from '@auth0/angular-jwt';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
+
 @NgModule({
   declarations: [	
     AppComponent,
@@ -23,6 +35,8 @@ import { MessagesComponent } from './messages/messages.component';
       RegisterComponent,
       ListsComponent,
       MemberListComponent,
+      MemberCardComponent,
+      MemberDetailComponent,
       MessagesComponent
    ],
   imports: [
@@ -30,10 +44,19 @@ import { MessagesComponent } from './messages/messages.component';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    NgxGalleryModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:5000'],
+        disallowedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
-  providers: [ErrorInterceptorProvider],
+  providers: [ErrorInterceptorProvider, MemberDetailResolver, MemberListResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using DatingApp.API.Models;
 
 namespace DatingApp.API.Dtos
 {
@@ -13,6 +12,9 @@ namespace DatingApp.API.Dtos
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public string PhotoUrl { get; set; }
